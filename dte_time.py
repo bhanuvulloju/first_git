643ab2c967463c01fdc591a8b6b6403e3e4e35a9
Bhanu <bhanu.vulloju@gmail.com>
2022-03-11 00:26:10 +0530

    my first commit

---
+++ dte_time.py
@@ -0,0 +1,26 @@
+import datetime
+
+tday = datetime.date.today()
+d1 = datetime.date(2001, 11, 16)
+
+# print(d1-d)
+td = datetime.date.today()
+# print(td)
+# print(td.year)
+
+tdelta = datetime.timedelta(days=7)
+
+# print(td + tdelta)
+
+bday = datetime.date(2001,11,18)
+till_bday = tday - bday
+# print(till_bday)
+
+
+# t = datetime.time(9,30,45,100000)
+# print(t.hour)
+
+t = datetime.datetime(2021,3,12,3,23,45,100000)
+print(t.date())
+print(t.time())
+
